f93f62d8dc149d58e44b813737eaa99900dceed5
Kiko <76973960+yesitskiko@users.noreply.github.com>
2023-12-12 14:17:28 +0100

    v50

---
+++ js/products.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+       // console.log(entry)
+        const squares = entry.target.querySelectorAll('.product');
+        //alert(entry.isIntersecting ? 'Intersecting' : 'Not Intersecting');
+        squares.forEach(square =>
+        {
+            if (entry.isIntersecting) {
+                square.classList.add('product-anim');
+                //return; // if we added the class, exit the function
+            }
+            else 
+            {
+                square.classList.remove('product-anim');
+            }
+        })
+
+        // We're not intersecting, so remove the class!
+        
+    });
+});
+observer.observe(document.querySelector('.products'));
+observer.observe(document.querySelector('.products2'));
--- js/script.js
+++ js/script.js
@@ -20,3 +20,4 @@ const observer = new IntersectionObserver(entries => {
     });
 });
 observer.observe(document.querySelector('.products'));
+observer.observe(document.querySelector('.products2'));
